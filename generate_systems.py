673552512fdfe79d195a44686beaefb77d86d12f
Daniel Cooper <stacksparrow4@gmail.com>
2024-05-28 18:07:36 +1000

    Refactor to have the llm queue as a seperate module

--- generate_systems.py
+++ generate_systems.py
@@ -2,9 +2,10 @@ import asyncio
 import os
 
 import llm
+import llm_queue
 import util
 
-from constants import USERS, LLAMA_HOSTS
+from constants import USERS
 
 user_names = [u[1] for u in USERS]
 name_to_pronoun = {}
@@ -24,23 +25,13 @@ def get_persona_info(persona, info_type):
 
 NUM_FACT_GENERATIONS = 10
 
-async def queue_worker(host, queue):
-    while len(queue) > 0:
-        f_path, system, prompt = queue.pop(0)
-        util.create_dir_if_not_exists(os.path.dirname(f_path))
-
-        res = await llm.generate(host, system, prompt)
-
-        with open(f_path, "w") as f:
-            f.write(res)
-
 async def main():
-    queue = []
+    queue = llm_queue.LLMQueue()
 
     for u in user_names:
         # Traits
         persona_traits = get_persona_info(u, 'traits')
-        queue.append((f"system/{u}/traits", "Summarise the character traits listed. Provide as much detail as possible.", persona_traits))
+        queue.enqueue_job((f"system/{u}/traits", "Summarise the character traits listed. Provide as much detail as possible.", persona_traits))
 
         # Facts
         facts = util.extract_dot_points(get_persona_info(u, 'facts'))
@@ -49,8 +40,10 @@ async def main():
         batch_size = len(fact_lines) // (NUM_FACT_GENERATIONS - 1)
 
         for i, batch in enumerate(util.generate_batches(fact_lines, batch_size)):
-            queue.append((f"system/{u}/facts/{i}", f"Repeat the below facts about {u}, removing any that are unimportant.", "\n".join(batch)))
+            queue.enqueue_job((f"system/{u}/facts/{i}", f"Repeat the below facts about {u}, removing any that are unimportant.", "\n".join(batch)))
     
-    await asyncio.gather(*[queue_worker(h, queue) for h in LLAMA_HOSTS])
+    await queue.process_queue(recompute=True)
+
+    await llm.close()
 
 asyncio.run(main())
---
+++ llm_queue.py
@@ -0,0 +1,47 @@
+import os
+import asyncio
+
+import llm
+import util
+
+from constants import LLAMA_HOSTS
+
+class LLMQueue:
+    def __init__(self, debug=True):
+        self.ind = 0
+        self.last_progress = 0
+        self.queue = []
+        self.debug = debug
+    
+    def enqueue_job(self, path, system, prompt):
+        self.queue.append((path, system, prompt))
+    
+    async def process_queue_with_host(self, host, recompute):
+        while len(self.queue) > 0:
+            path, system, prompt = self.queue[self.ind]
+            self.ind += 1
+
+            util.create_dir_if_not_exists(os.path.dirname(path))
+
+            curr_prog = (100 * self.ind) // len(self.queue)
+
+            if recompute or not os.path.isfile(path):
+                res = await llm.generate(host, system, prompt)
+
+                if self.debug:
+                    print("====================")
+                    print(path)
+                    print(res)
+
+                with open(path, "w") as f:
+                    f.write(res)
+                
+                if curr_prog != self.last_progress:
+                    print(f"{curr_prog}%")
+            
+            self.last_progress = curr_prog
+
+    async def process_queue(self, recompute=False):
+        assert self.ind == 0
+
+        await asyncio.gather(*[self.process_queue_with_host(host, recompute) for host in LLAMA_HOSTS])
