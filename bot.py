896757033b1ffb21a5c6aa4531c220d1eba5842e
Daniel Cooper <stacksparrow4@gmail.com>
2024-05-30 18:01:11 +1000

    Added conversation history

--- bot.py
+++ bot.py
@@ -23,33 +23,60 @@ async def on_ready():
     
     await llm.setup()
 
+MAX_MESSAGE_HIST = 100
+
+# Structure: [(message_id, persona, [(user_msg, bot_msg), ...]), ...]
+msg_hist = []
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    chosen_persona = None
+    history = None
+    prompt = ""
+    if message.reference is not None:
+        matches = [m for m in msg_hist if m[0] == message.reference.message_id]
 
-    if not message.content.startswith(msg_prefix):
-        return
+        if len(matches) > 0:
+            chosen_persona = matches[0][1]
+            history = matches[0][2]
+            prompt = message.content
+        else:
+            return
+    else:
+        if not message.content.startswith(msg_prefix):
+            return
 
-    if not message.channel.id in whitelisted_channels:
-        return
+        if not message.channel.id in whitelisted_channels:
+            return
 
-    parts = message.content.split(maxsplit=2)
-    if len(parts) == 3:
-        chosen_persona = parts[1]
-        
-        if not chosen_persona in os.listdir("collated"):
-            await message.reply("Available personas: " + " ".join(os.listdir("collated")))
+        parts = message.content.split(maxsplit=2)
+        if len(parts) != 3:
+            await message.reply(f"Usage: {msg_prefix} <PERSONA> <PROMPT>")
             return
         
-        async with message.channel.typing():
-            resp = await persona.respond_as_persona(chosen_persona, parts[2])
+        chosen_persona = parts[1]
+        prompt = parts[2]
         
-        for emoji in message.guild.emojis:
-            resp = resp.replace(f":{emoji.name}:", f"<:{emoji.name}:{emoji.id}>")
+    if not chosen_persona in os.listdir("collated"):
+        await message.reply("Available personas: " + " ".join(os.listdir("collated")))
+        return
+    
+    async with message.channel.typing():
+        resp = await persona.respond_as_persona(chosen_persona, prompt, history)
+    
+    emojified_resp = resp
 
-        await message.reply(resp)
-    else:
-        await message.reply(f"Usage: {msg_prefix} <PERSONA> <PROMPT>")
+    for emoji in message.guild.emojis:
+        emojified_resp = emojified_resp.replace(f":{emoji.name}:", f"<:{emoji.name}:{emoji.id}>")
+
+    sent_msg = await message.reply(emojified_resp)
+
+    while len(msg_hist) >= MAX_MESSAGE_HIST:
+        msg_hist.pop(0)
+    
+    msg_hist.append((sent_msg.id, chosen_persona, ([] if history is None else history) + [(prompt, resp)]))
 
 client.run(os.environ.get("BOT_TOKEN"))
--- persona.py
+++ persona.py
@@ -24,7 +24,7 @@ async def get_persona_facts(persona):
 async def get_persona_examples(persona):
     return "\n".join(random.sample([m[1] for m in msg_db if m[0] == persona], NUM_RANDOM_EXAMPLES))
 
-async def respond_as_persona(persona, prompt):
+async def respond_as_persona(persona, prompt, history=None):
     system = f"""You are pretending to be the person '{persona}'. Below is a list of facts about '{persona}':
 
 {await get_persona_facts(persona)}
@@ -35,6 +35,14 @@ Below is a list of example messages sent by '{persona}':
 
 Respond to the following message as if you were '{persona}'. Remember to copy the texting style of '{persona}' as shown in the examples."""
     
+    if history:
+        system += "\nThis conversation has the following history:"
+        for h in history:
+            system += "\nThe user said '" + h[0] + "'"
+            system += "\nYou said '" + h[1] + "'"
+    
     prompt = messages.clean_msg(prompt, user_db)
+
+    print(system)
 
     return await llm.generate(util.get_llama_hosts()[0], system, prompt)
